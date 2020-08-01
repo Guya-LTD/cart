837db980dbad1fc2bd2dd240701195ea7d23d065
semantic-release <semantic-release>
2020-08-01 03:40:09 +0300

    0.1.0
    Automatically generated by python-semantic-release

--- cart/__init__.py
+++ cart/__init__.py
@@ -54,7 +54,7 @@ from .handlers import register_handler
 from cart.controller.v1 import foo_controller
 
 # const vars
-__version__ = '0.0.1'
+__version__ = '0.1.0'
 __email__ = 'simonbelete@gmail.com'
 __title__ = 'Cart service for Guya microservices'
 __author__ = 'Simon Belete'
